546939f242815f78f4e0386e4fb33c00f5c135e2
Mks1311 <manish31311@gmail.com>
2024-03-23 01:25:06 +0530

    Update script.js
    Removed an error where DEL was not working after using equals button

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-let TextOnScreen=document.querySelector("#screen");
-let buttons=document.querySelectorAll("button");
-let expression="";
-buttons.forEach((button)=>{
-    button.addEventListener('click',(event)=>{
-        let className=(event.target.className);
-        let CurrText=event.target.innerHTML;
-        let lastText=TextOnScreen.value[TextOnScreen.value.length-1];
-        if(className==="operator" && (lastText==='+' || lastText==='-' || lastText==='*' || lastText==='/' || lastText==='%')){}
-        else if(CurrText==="AC"){
-            expression="";
-            TextOnScreen.value="";
+let TextOnScreen = document.querySelector("#screen");
+let buttons = document.querySelectorAll("button");
+let expression = "";
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        let className = (event.target.className);
+        let CurrText = event.target.innerHTML;
+        let lastText = TextOnScreen.value[TextOnScreen.value.length - 1];
+        if (className === "operator" && (lastText === '+' || lastText === '-' || lastText === '*' || lastText === '/' || lastText === '%')) { }
+        else if (CurrText === "AC") {
+            expression = "";
+            TextOnScreen.value = "";
         }
-        else if(CurrText==="="){
-            expression=eval(expression)
-            TextOnScreen.value=expression;
+        else if (CurrText === "=") {
+            expression = eval(expression);
+            TextOnScreen.value = expression;
+            console.log(expression);
         }
-        else if(CurrText==="DEL"){
-            expression=expression.slice(0, -1);
-            TextOnScreen.value=expression;
+        else if (CurrText === "DEL") {
+            TextOnScreen.value=TextOnScreen.value.slice(0,-1);
+            expression=TextOnScreen.value
         }
-        else{
-            expression+=CurrText;
-            TextOnScreen.value+=CurrText;
+        else {
+            expression += CurrText;
+            TextOnScreen.value += CurrText;
         }
     })
 })
